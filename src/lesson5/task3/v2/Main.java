7306cdd354ffaf7a4d6918ca3f53391c4ebce07b
GHTeodor <Theodor.Lviv@gmail.com>
2022-06-23 19:44:32 +0300

    task3 one more solution

--- src/lesson5/task3/v2/Main.java
+++ src/lesson5/task3/v2/Main.java
@@ -7,7 +7,6 @@ import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
